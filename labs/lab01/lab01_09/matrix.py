8e60a806aa788a4c28aa9434f5fdb34ac7ec2563
ovs2004 <113552216+ovs2004@users.noreply.github.com>
2023-09-24 00:47:41 +0300

    matrix_func3

--- labs/lab01/lab01_09/matrix.py
+++ labs/lab01/lab01_09/matrix.py
@@ -21,12 +21,12 @@ def timing_decorator(ndigits: int, number: int) -> typing.Callable:
     return decorator
 
 
-max_n = 1000
+max_n = 1112
 
 ndigits = 6
 number_of_runs = 5
 
-n_values = list(range(1, max_n, 10))
+n_values = list(range(1, max_n, 11))
 average_times = []
 
 
@@ -38,8 +38,8 @@ def matrix_multiply(matrix_A, matrix_B):
 for n in n_values:
     print(n)
 
-    matrix_A = np.random.randint(1, 2, size=(n, n))
-    matrix_B = np.random.randint(1, 2, size=(n, n))
+    matrix_A = np.random.randint(1, 100, size=(n, n))
+    matrix_B = np.random.randint(1, 100, size=(n, n))
 
     average_time = matrix_multiply(matrix_A, matrix_B)
     average_times.append(average_time)
