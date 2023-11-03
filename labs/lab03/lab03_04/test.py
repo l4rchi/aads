e4fce8a1347537bcf7f89d362f22b4a2c07187d2
Vadushez <kopalka1998@gmail.com>
2023-11-03 17:59:25 +0300

    dialogue window added

--- labs/lab03/lab03_04/test.py
+++ labs/lab03/lab03_04/test.py
@@ -1,33 +1,44 @@
-from doublylinkedlist import Node, DoublyLinkedList
+from doublylinkedlist import DoublyLinkedList
+my_list = DoublyLinkedList()
+print("Выберите действие:")
+print("1. Добавить элемент")
+print("2. Вывести список")
+print("3. Перевернуть список")
+print("4. Удалить элемент с конца")
+print("5. Удалить элемент с начала")
+print("6. Удалить элемент после значения")
+print(
+    "7. Удалить элемент перед значением")
+print("8. по списку L строит два новых списка: L1 – из положительных элементов и L2 – из отрицательных элементов списка L.")
+print("9. Продублировать все простые числа")
+print("10. Выйти.")
+while True:
+    choice = input("Введите номер действия: ")
 
-a = DoublyLinkedList()
-a.push_front(5)
-a.push_front(3)
-a.push_front(6)
-a.push_front(7)
-a.push_back(-1)
-a.print_linkedlist()
-print('haha')
-a.reverse_list()
-a.print_linkedlist()
-print('haha')
-a.push_before(232,2)
-a.print_linkedlist()
-print('haha')
-a.pop(2)
-a.print_linkedlist()
-print('haha')
-a.pop_before(2)
-a.print_linkedlist()
-print('haha')
-b, c = a.listsplit()
-b.print_linkedlist()
-print('haha')
-c.print_linkedlist()
-print('hahahh')
-a.print_linkedlist()
-print('haha')
-a.simplenum()
-
-
-
+    if choice == '1':
+        data = int(input("Введите элемент для добавления: "))
+        my_list.push_back(data)
+        print("Элемент добавлен в список.")
+    elif choice == '2':
+        print("Содержимое списка:")
+        my_list.print_linkedlist()
+    elif choice == '3':
+        print("Перевернутый список:")
+        my_list.reverse_list()
+        my_list.print_linkedlist()
+    elif choice == '4':
+        my_list.pop_back()
+    elif choice == '5':
+        my_list.pop_front()
+    elif choice == '6':
+        index = int(input("Введите индекс: "))
+        my_list.pop(index)
+    elif choice == '7':
+        my_list.pop_before()
+    elif choice == '8':
+        my_list.listsplit()
+    elif choice == '9':
+        print("Простые числа:")
+        my_list.simplenum()
+    elif choice == '10':
+        break
